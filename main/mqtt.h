c41037f3a88ceefd94b733513d3a261ffdae3978
fussionko <sebi.vidergar@gmail.com>
2023-08-03 21:14:37 +0200

    updated to seperate files

---
+++ main/mqtt.h
@@ -0,0 +1,9 @@
+#ifndef _MQTT_H__
+#define _MQTT_H__
+
+//#define CONFIG_BROKER_URL "mqtt://sebi:admin@192.168.1.4:1883"
+
+void mqtt_init(void);
+void mqtt_start(void);
+
+#endif // _MQTT_H__
---
+++ main/wifi.h
@@ -0,0 +1,10 @@
+#ifndef _WIFI_H__
+#define _WIFI_H__
+
+#define EXAMPLE_ESP_WIFI_SSID      "SebiWi2Fi"
+#define EXAMPLE_ESP_WIFI_PASS      "Fbtksd11"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  3
+
+void wifi_init_sta(void);
+
+#endif // _WIFI_H__
